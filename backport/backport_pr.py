8f0c1e0ddc7b63bcba1d7f85d4bad3c838a63abd
Mariatta Wijaya <mariatta.wijaya@gmail.com>
2017-08-20 20:29:01 -0700

    print?

--- backport/backport_pr.py
+++ backport/backport_pr.py
@@ -1,11 +1,12 @@
 import gidgethub.routing
 import cherry_picker
-
-
+import os
+import subprocess
 router = gidgethub.routing.Router()
 
 @router.register("pull_request", action="opened")
 async def backport_pr(event, gh, *args, **kwargs):
-    print(event)
-    print(gh)
+    os.chdir("cpython")
+    subprocess.check_output("git status".split())
+    subprocess.check_output("git remote --v".split())
     pass
