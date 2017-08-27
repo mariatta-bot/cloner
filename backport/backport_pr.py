ef35631f19a68ce84666173225ea6f6d83c0e2f8
Mariatta Wijaya <mariatta.wijaya@gmail.com>
2017-08-26 22:17:48 -0700

    routing and printing

--- backport/backport_pr.py
+++ backport/backport_pr.py
@@ -10,6 +10,12 @@ async def backport_pr(event, gh, *args, **kwargs):
     print(event.data)
     pass
 
+@router.register("pull_request", action="reopened")
+async def backport_pr(event, gh, *args, **kwargs):
+    print(" pr reopened ")
+    print(event.data)
+    pass
+
 @router.register("pull_request", action="closed")
 async def backport_pr(event, gh, *args, **kwargs):
     print(" pr merged ")
