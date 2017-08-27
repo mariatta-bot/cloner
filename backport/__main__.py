a30d34fb4edc50a1f46b8fa4a1ee364e9683989c
Mariatta Wijaya <mariatta.wijaya@gmail.com>
2017-08-26 21:31:15 -0700

    clone later

--- backport/__main__.py
+++ backport/__main__.py
@@ -28,9 +28,9 @@ async def main(request):
         #     result = subprocess.check_output("git clone https://github.com/mariatta/cpython.git".split())
         #
         #     print(result.decode('utf-8'))
-        os.chdir('cpython')
+        os.chdir('strange-relationship')
         # print(os.listdir('.'))
-        print(subprocess.check_output("git remote --v".split()).decode('utf-8'))
+        print(subprocess.check_output("git status".split()).decode('utf-8'))
         # result = subprocess.check_output("git remote add upstream https://github.com/mariatta/cpython.git".split())
         # print(result.decode('utf-8'))
         # print(subprocess.check_output("git remote --v".split()).decode('utf-8'))
@@ -57,7 +57,7 @@ async def main(request):
         traceback.print_exc(file=sys.stderr)
         return web.Response(status=500)
 
-def clone_cpython():
+async def clone_cpython():
     """ Clone CPython, and set upstream remote"""
     if not os.path.isdir("cpython"):
         print("cloning")
@@ -77,6 +77,7 @@ if __name__ == "__main__":  # pragma: no cover
     if port is not None:
         port = int(port)
     web.run_app(app, port=port)
-    clone_cpython()
+
+    # clone_cpython()
 
 
