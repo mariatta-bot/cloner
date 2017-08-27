7120a1e5ebde653824243e4614920453cef47560
Mariatta Wijaya <mariatta.wijaya@gmail.com>
2017-08-26 21:14:08 -0700

    set remote url

--- backport/__main__.py
+++ backport/__main__.py
@@ -29,11 +29,11 @@ async def main(request):
         #
         #     print(result.decode('utf-8'))
         os.chdir('cpython')
-        print(os.listdir('.'))
+        # print(os.listdir('.'))
         print(subprocess.check_output("git remote --v".split()).decode('utf-8'))
-        result = subprocess.check_output("git remote add upstream https://github.com/mariatta/cpython.git".split())
-        print(result.decode('utf-8'))
-        print(subprocess.check_output("git remote --v".split()).decode('utf-8'))
+        # result = subprocess.check_output("git remote add upstream https://github.com/mariatta/cpython.git".split())
+        # print(result.decode('utf-8'))
+        # print(subprocess.check_output("git remote --v".split()).decode('utf-8'))
         # print("done check output")
         # print(subprocess.check_output("git log 8ccc723920ee001fea48f5ede8b721c7f96f473d".split()).decode('utf-8'))
         os.chdir('..')
@@ -58,11 +58,15 @@ async def main(request):
         return web.Response(status=500)
 
 def clone_cpython():
+    """ Clone CPython, and set upstream remote"""
     if not os.path.isdir("cpython"):
         print("cloning")
         result = subprocess.check_output(
             "git clone https://github.com/mariatta/cpython.git".split())
 
+        print(result.decode('utf-8'))
+
+        result = subprocess.check_output("git remote add upstream https://github.com/mariatta/cpython.git".split())
         print(result.decode('utf-8'))
 
 if __name__ == "__main__":  # pragma: no cover
